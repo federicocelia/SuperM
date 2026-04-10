521a494b956fb6060d1557778397f58300542e17
Federico Celia <73991066+federicocelia@users.noreply.github.com>
2026-04-10 02:17:49 +0100

    initial files set up
    creating first product component

---
+++ react-supermarket-website/src/main.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createRoot } from "react-dom/client";
+
+// Product component
+function Product() {
+  return (
+    <div className="product">
+      <img
+        className="product-image"
+        width="272"
+        height="300"
+        alt="Pineapple"
+        src="https://res.cloudinary.com/dbfn5lnvx/image/upload/v1726640668/react-tutorial/superm-v2/pineapple.jpg"
+      />
+      <p className="product-name">Pineapple</p>
+      <div className="product-price">$2.00</div>
+    </div>
+  );
+}
+
+createRoot(document.querySelector("#root")).render(<Product />);

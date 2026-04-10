4ff1207d24a6c4600586655d775887221fdaec67
Federico Celia <73991066+federicocelia@users.noreply.github.com>
2026-04-10 18:38:32 +0100

    refactor: move Product component into Product.jsx and add StrictMode

--- react-supermarket-website/src/main.jsx
+++ react-supermarket-website/src/main.jsx
@@ -1,24 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { StrictMode } from "react";
 import "./index.css";
 import { createRoot } from "react-dom/client";
-
-// Product component
-function Product() {
-  return (
-    <div className="product">
-      <img
-        className="product-image"
-        width="272"
-        height="300"
-        alt="Pineapple"
-        src="https://res.cloudinary.com/dbfn5lnvx/image/upload/v1726640668/react-tutorial/superm-v2/pineapple.jpg"
-      />
-      <p className="product-name">Pineapple</p>
-      <div className="product-price">$2.00</div>
-    </div>
-  );
-}
+import Product from "./Product";
 
 function App() {
   // Checking the rendering of multiple products inside the .products-grid
@@ -32,4 +17,8 @@ function App() {
   );
 }
 
-createRoot(document.querySelector("#root")).render(<App />);
+createRoot(document.querySelector("#root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
